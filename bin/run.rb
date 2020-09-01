dbfb0e978079aa633b0fd6defe88d29d0cf3b634
Cristian Deleon <libaration@gmail.com>
2020-09-01 00:52:11 -0400

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -3,5 +3,6 @@ require_relative "environment"
 Scraper.new(@db).scrape
 
 all_pokemon = @db.execute("SELECT * FROM pokemon;")
+pp Pokemon.find_type('Electric', @db)
 
 # test out your code here!
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id: nil, name:, type:, db:)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+  end
+
+  def self.save(name,type,db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name,type)
+    VALUES (?,?)
+    SQL
+    db.execute(sql,name,type)
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+    SELECT *
+    FROM pokemon
+    WHERE id IS ?
+    SQL
+    selected = db.execute(sql, id).flatten
+    self.new(id: selected[0], name: selected[1], type: selected[2], db: db)
+  end
+
+  def self.find_type(type,db)
+    sql = <<-SQL
+    SELECT *
+    FROM pokemon
+    WHERE type IS ?
+    SQL
+    db.execute(sql,type)
+  end
+
+
 end
